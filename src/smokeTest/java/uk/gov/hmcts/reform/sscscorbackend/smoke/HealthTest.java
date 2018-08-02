99e6339b2255c2d847cd45406114398c4d8b152e
Chris Grimble <chris@cg-it-solutions.com>
2018-08-02 16:27:32 +0100

    Adding smoke test

--- src/smokeTest/java/uk/gov/hmcts/reform/sscscorbackend/smoke/HealthTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sscscorbackend/smoke/HealthTest.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.sscscorbackend.smoke;
 
-import io.restassured.RestAssured;
 import org.junit.Test;
-import org.springframework.http.HttpStatus;
 
 public class HealthTest {
     private final String baseUrl = System.getenv("TEST_URL");
