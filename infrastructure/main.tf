507fee53a5555c6bfc63ee6b5fc8864c4cc77648
Chris Grimble <chris@cg-it-solutions.com>
2018-08-02 12:29:32 +0100

    Fixing build

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,7 +10,7 @@ locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 }
 
-module "" {
+module "sscs-core-backend" {
   source              = "git@github.com:hmcts/moj-module-webapp?ref=master"
   product             = "${var.product}-${var.component}"
   location            = "${var.location_app}"
@@ -19,6 +19,7 @@ module "" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
   subscription        = "${var.subscription}"
   capacity            = "${var.capacity}"
+  common_tags         = "${var.common_tags}"
 
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
