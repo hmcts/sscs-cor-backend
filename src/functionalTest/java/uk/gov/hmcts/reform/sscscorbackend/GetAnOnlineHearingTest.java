b5698a64d98434fde8a7f9277d7250c218bc1657
David Thompson <davethompson21@gmail.com>
2018-09-26 10:19:47 +0100

    Adding functional test for getting online hearing with decision
    * there is a problem with issuing the decision which needs to be resolved
    * need to add the decision state to the JSON and check that also

--- src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/GetAnOnlineHearingTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/GetAnOnlineHearingTest.java
@@ -1,24 +1,58 @@
 package uk.gov.hmcts.reform.sscscorbackend;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
-import java.util.UUID;
 import org.json.JSONObject;
 import org.junit.Test;
 
 public class GetAnOnlineHearingTest extends BaseFunctionTest {
+    private final String DECISION_AWARD = "FINAL";
+    private final String DECISION_HEADER = "The decision";
+    private final String DECISION_REASON = "Decision reason";
+    private final String DECISION_TEXT = "The decision text";
+
     @Test
     public void getAnOnlineHearing() throws IOException {
-        String emailAddress = "someEmail" + UUID.randomUUID().toString() + "@example.com";
+        int randomNumber = (int) (Math.random() * 10000000);
+        String emailAddress = "test" + String.valueOf(randomNumber) + "@hmcts.net";
         System.out.println("emailAddress " + emailAddress);
         String caseId = sscsCorBackendRequests.createCase(emailAddress);
         String expectedOnlineHearingId = cohRequests.createHearing(caseId);
 
         JSONObject onlineHearing = sscsCorBackendRequests.getOnlineHearing(emailAddress);
         String onlineHearingId = onlineHearing.getString("online_hearing_id");
+        JSONObject decision = onlineHearing.optJSONObject("decision");
 
         assertThat(onlineHearingId, is(expectedOnlineHearingId));
+        assertThat(decision, is(nullValue()));
+    }
+
+    @Test
+    public void getAnOnlineHearingWithDecision() throws IOException, InterruptedException {
+        int randomNumber = (int) (Math.random() * 10000000);
+        String emailAddress = "test" + String.valueOf(randomNumber) + "@hmcts.net";
+        System.out.println("emailAddress " + emailAddress);
+        String caseId = sscsCorBackendRequests.createCase(emailAddress);
+        String expectedOnlineHearingId = cohRequests.createHearing(caseId);
+        String questionId = cohRequests.createQuestion(expectedOnlineHearingId);
+        cohRequests.issueQuestionRound(expectedOnlineHearingId);
+        String answerId = cohRequests.createAnswer(expectedOnlineHearingId, questionId, "Valid answer");
+        String decisionId = cohRequests.createDecision(expectedOnlineHearingId, DECISION_AWARD,
+                DECISION_HEADER, DECISION_REASON, DECISION_TEXT);
+        cohRequests.issueDecision(expectedOnlineHearingId, DECISION_AWARD,
+                DECISION_HEADER, DECISION_REASON, DECISION_TEXT);
+
+        JSONObject onlineHearing = sscsCorBackendRequests.getOnlineHearing(emailAddress);
+        String onlineHearingId = onlineHearing.getString("online_hearing_id");
+        String decisionAward = onlineHearing.getJSONObject("decision").getString("decision_award");
+
+        assertThat(onlineHearingId, is(expectedOnlineHearingId));
+        assertThat(onlineHearing.getJSONObject("decision").getString("decision_award"), is(DECISION_AWARD));
+        assertThat(onlineHearing.getJSONObject("decision").getString("decision_header"), is(DECISION_HEADER));
+        assertThat(onlineHearing.getJSONObject("decision").getString("decision_reason"), is(DECISION_REASON));
+        assertThat(onlineHearing.getJSONObject("decision").getString("decision_text"), is(DECISION_TEXT));
     }
 }
