8a4e73aebd935a64ef90220266280f79f04d5539
Chris Grimble <chris@cg-it-solutions.com>
2018-10-24 16:59:45 +0100

    SSCS-4168 List evidence uploads
    Add test for listing evidence as part of the question.

--- src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/EvidenceUploadTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/EvidenceUploadTest.java
@@ -1,13 +1,29 @@
 package uk.gov.hmcts.reform.sscscorbackend;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class EvidenceUploadTest extends BaseFunctionTest {
     @Test
-    public void uploadEvidence() throws IOException, InterruptedException {
+    public void uploadEvidence() throws IOException, InterruptedException, JSONException {
         OnlineHearing hearingWithQuestion = createHearingWithQuestion(true);
 
-        sscsCorBackendRequests.uploadEvidence(hearingWithQuestion.getHearingId(), hearingWithQuestion.getQuestionId());
+        JSONObject questionResponse = sscsCorBackendRequests.getQuestion(hearingWithQuestion.getHearingId(), hearingWithQuestion.getQuestionId());
+        assertThat(questionResponse.has("evidence"), is(false));
+
+        String fileName = "evidence.png";
+        sscsCorBackendRequests.uploadEvidence(hearingWithQuestion.getHearingId(), hearingWithQuestion.getQuestionId(), fileName);
+        sscsCorBackendRequests.answerQuestion(hearingWithQuestion.getHearingId(), hearingWithQuestion.getQuestionId(), "some answer");
+
+        questionResponse = sscsCorBackendRequests.getQuestion(hearingWithQuestion.getHearingId(), hearingWithQuestion.getQuestionId());
+        JSONArray evidence = questionResponse.getJSONArray("evidence");
+        assertThat(evidence.length(), is(1));
+        assertThat(evidence.getJSONObject(0).getString("file_name"), is(fileName));
     }
 }
