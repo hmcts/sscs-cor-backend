a2b9cb1e5ffadb23415ad782d85a046616b36d18
Chris <christopher.davidson@btinternet.com>
2018-11-01 20:34:46 +0000

    fixed checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/CreateHearingPdfTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/CreateHearingPdfTest.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.reform.sscscorbackend;
 
 import java.io.IOException;
-
-import org.junit.Assert;
-import org.junit.Test;
+//import org.junit.Test;
 
 public class CreateHearingPdfTest extends BaseFunctionTest {
 
