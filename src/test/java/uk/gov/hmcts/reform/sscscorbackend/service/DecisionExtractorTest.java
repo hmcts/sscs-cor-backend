503f6f084fb70e00b125e50d995a4fbc0f7e0cac
Chris Grimble <chris@cg-it-solutions.com>
2018-12-21 11:59:14 +0000

    SSCS-4818 Preliminary View
    The pannel may not set an end date for the their decision. In
    which case we don't want to send an endate to the frontend.

--- src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/DecisionExtractorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/DecisionExtractorTest.java
@@ -26,7 +26,7 @@ public class DecisionExtractorTest {
             "      \\\"awardEndDateDay\\\":\\\"11\\\",\n" +
             "      \\\"awardEndDateMonth\\\":\\\"12\\\",\n" +
             "      \\\"awardEndDateYear\\\":\\\"2018\\\",\n" +
-            "      \\\"approveDraftConsent\\\":\\\"indefinite\\\",\n" +
+            "      \\\"approveDraftConsent\\\":\\\"endDate\\\",\n" +
             "      \\\"preparingFood\\\":false,\n" +
             "      \\\"takingNutrition\\\":false,\n" +
             "      \\\"managingTherapy\\\":false,\n" +
@@ -48,19 +48,58 @@ public class DecisionExtractorTest {
             "   }\n" +
             "}";
 
+    private static final String decisionWithNoEndDateString = "{  \n" +
+            "   \\\"decisions_SSCS_benefit_1234321\\\":{  \n" +
+            "      \\\"preliminaryView\\\":\\\"yes\\\",\n" +
+            "      \\\"visitedPages\\\":{  \n" +
+            "         \\\"create\\\":true,\n" +
+            "         \\\"preliminary-advanced\\\":true,\n" +
+            "         \\\"set-award-dates\\\":true,\n" +
+            "         \\\"scores\\\":true,\n" +
+            "         \\\"budgeting-decisions\\\":true,\n" +
+            "         \\\"planning-journeys\\\":true\n" +
+            "      },\n" +
+            "      \\\"forDailyLiving\\\":\\\"noAward\\\",\n" +
+            "      \\\"forMobility\\\":\\\"enhancedRate\\\",\n" +
+            "      \\\"compareToDWPAward\\\":\\\"Higher\\\",\n" +
+            "      \\\"awardEndDateDay\\\":\\\"null\\\",\n" +
+            "      \\\"awardEndDateMonth\\\":\\\"null\\\",\n" +
+            "      \\\"awardEndDateYear\\\":\\\"null\\\",\n" +
+            "      \\\"approveDraftConsent\\\":\\\"indefinite\\\",\n" +
+            "      \\\"preparingFood\\\":false,\n" +
+            "      \\\"takingNutrition\\\":false,\n" +
+            "      \\\"managingTherapy\\\":false,\n" +
+            "      \\\"washingBathing\\\":false,\n" +
+            "      \\\"managingToilet\\\":false,\n" +
+            "      \\\"dressingUndressing\\\":false,\n" +
+            "      \\\"communicatingVerbally\\\":false,\n" +
+            "      \\\"readingAndUnderstanding\\\":false,\n" +
+            "      \\\"engagingWithOtherPeople\\\":false,\n" +
+            "      \\\"makingBudgetingDecisions\\\":true,\n" +
+            "      \\\"planningFollowingJourneys\\\":true,\n" +
+            "      \\\"movingAround\\\":false,\n" +
+            "      \\\"dailyLivingMakingBudgetDecisions\\\":\\\"6\\\",\n" +
+            "      \\\"MobilityPlanningJourneys\\\":\\\"12\\\",\n" +
+            "      \\\"reasonsTribunalView\\\":\\\"There was a reason!\\\",\n" +
+            "      \\\"awardStartDateDay\\\":\\\"1\\\",\n" +
+            "      \\\"awardStartDateMonth\\\":\\\"4\\\",\n" +
+            "      \\\"awardStartDateYear\\\":\\\"2017\\\"\n" +
+            "   }\n" +
+            "}";
+    private final String decisionsState = "decisionsState";
+    private final String decisionsStartDateTime = "decisionsStartDateTime";
+    private final String appellantReply = "appellantReply";
+    private final String appellantReplyDateTime = "appellantReplyDateTime";
+    private final CohDecisionReply cohDecisionReply = new CohDecisionReply(
+            appellantReply, "reason", appellantReplyDateTime, "authorRef"
+    );
+
+
     //todo handle line breaks in reasons
     @Test
     public void extractDecision() {
-        String decisionsState = "decisionsState";
-        String decisionsStartDateTime = "decisionsStartDateTime";
-        String appellantReply = "appellantReply";
-        String appellantReplyDateTime = "appellantReplyDateTime";
-
         CohDecision cohDecision = new CohDecision(
                 "onlineHearingId", "", "", "", decisionString, new CohState(decisionsState, decisionsStartDateTime)
-        );
-        CohDecisionReply cohDecisionReply = new CohDecisionReply(
-                appellantReply, "reason", appellantReplyDateTime, "authorRef"
         );
         Decision decision = new DecisionExtractor().extract(caseId, cohDecision, cohDecisionReply);
 
@@ -71,6 +110,29 @@ public class DecisionExtractorTest {
                 appellantReplyDateTime,
                 "2017-04-01",
                 "2018-12-11",
+                new DecisionRates(Rate.noAward, Rate.enhancedRate, ComparedRate.Higher),
+                "There was a reason!",
+                new Activities(
+                        asList(new Activity("makingBudgetingDecisions", "6")),
+                        asList(new Activity("planningFollowingJourneys", "12"))
+                ))
+        ));
+    }
+
+    @Test
+    public void extractDecisionWithNoEndDate() {
+        CohDecision cohDecision = new CohDecision(
+                "onlineHearingId", "", "", "", decisionWithNoEndDateString, new CohState(decisionsState, decisionsStartDateTime)
+        );
+        Decision decision = new DecisionExtractor().extract(caseId, cohDecision, cohDecisionReply);
+
+        assertThat(decision, is(new Decision(
+                decisionsState,
+                decisionsStartDateTime,
+                appellantReply,
+                appellantReplyDateTime,
+                "2017-04-01",
+                null,
                 new DecisionRates(Rate.noAward, Rate.enhancedRate, ComparedRate.Higher),
                 "There was a reason!",
                 new Activities(
