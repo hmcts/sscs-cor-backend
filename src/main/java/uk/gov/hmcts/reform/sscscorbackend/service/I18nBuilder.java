1dff290b186ef50038ae998f46170ffb08ebe37e
Chris Grimble <chris@cg-it-solutions.com>
2019-01-07 10:32:43 +0000

    Fix build

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/I18nBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/I18nBuilder.java
@@ -4,12 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Map;
 import org.apache.poi.util.IOUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class I18nBuilder {
-    public HashMap build() throws IOException {
+    public Map build() throws IOException {
         InputStream in = getClass().getResourceAsStream("/json/en.json");
         return new ObjectMapper().readValue(IOUtils.toByteArray(in), HashMap.class);
     }
--- src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/I18nBuilderTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/I18nBuilderTest.java
@@ -4,13 +4,13 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Map;
 import org.junit.Test;
 
 public class I18nBuilderTest {
     @Test
     public void canLoadI18n() throws IOException {
-        HashMap i18n = new I18nBuilder().build();
+        Map i18n = new I18nBuilder().build();
 
         assertThat(i18n.containsKey("tribunalView"), is(true));
     }
