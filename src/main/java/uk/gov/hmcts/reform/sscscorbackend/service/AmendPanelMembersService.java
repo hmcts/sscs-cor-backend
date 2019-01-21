cd9b708193354e8d5eac8db8154a9da8943a1279
Chris Grimble <chris@cg-it-solutions.com>
2019-01-21 13:25:54 +0000

    Removed logging users who were being added or removed from a case.

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/AmendPanelMembersService.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/AmendPanelMembersService.java
@@ -4,14 +4,12 @@ import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscscorbackend.thirdparty.ccd.CorCcdService;
 import uk.gov.hmcts.reform.sscscorbackend.thirdparty.ccd.apinotifications.CaseData;
 import uk.gov.hmcts.reform.sscscorbackend.thirdparty.ccd.apinotifications.CaseDetails;
 import uk.gov.hmcts.reform.sscscorbackend.thirdparty.ccd.apinotifications.CcdEvent;
 
-@Slf4j
 @Service
 public class AmendPanelMembersService {
     private final CorCcdService ccdService;
@@ -24,12 +22,10 @@ public class AmendPanelMembersService {
         CaseDetails newCaseDetails = ccdEvent.getCaseDetails();
         CaseData newCase = newCaseDetails.getCaseData();
         List<String> membersToAddPermissionTo = new ArrayList<>(asList(newCase.getAssignedToDisabilityMember(), newCase.getAssignedToMedicalMember()));
-        log.info("PANEL MEMBERS New panel members [" + membersToAddPermissionTo + "]");
         long caseId = Long.parseLong(newCaseDetails.getCaseId());
         if (ccdEvent.getCaseDetailsBefore() != null && ccdEvent.getCaseDetailsBefore().getCaseData() != null) {
             CaseData oldCaseDetails = ccdEvent.getCaseDetailsBefore().getCaseData();
             List<String> membersToRemovePermissionsFrom = new ArrayList<>(asList(oldCaseDetails.getAssignedToDisabilityMember(), oldCaseDetails.getAssignedToMedicalMember()));
-            log.info("PANEL MEMBERS  Old panel members [" + membersToRemovePermissionsFrom + "]");
             membersToRemovePermissionsFrom.removeAll(membersToAddPermissionTo);
             membersToAddPermissionTo.remove(oldCaseDetails.getAssignedToDisabilityMember());
             membersToAddPermissionTo.remove(oldCaseDetails.getAssignedToMedicalMember());
