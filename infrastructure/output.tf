7f7f8d77d6c02f1dad33e4ea16735c81dc61f7e3
Chris <christopher.davidson@btinternet.com>
2019-01-31 16:46:12 +0000

    new way of loading secrets

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -6,10 +6,6 @@ output "vaultName" {
   value = "${local.azureVaultName}"
 }
 
-output "vaultUri" {
-  value = "${data.azurerm_key_vault.sscs_key_vault.vault_uri}"
-}
-
 output "idam_s2s_auth" {
   value = "http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 }
