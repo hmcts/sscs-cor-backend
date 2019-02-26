124c3c9f7d3265ba2ec16fce52a3d1267384301b
Chris <christopher.davidson@btinternet.com>
2019-02-26 09:44:10 +0000

    removed old vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,10 +8,6 @@ resource "azurerm_resource_group" "rg" {
   location = "${var.location_app}"
 }
 
-provider "vault" {
-  address = "https://vault.reform.hmcts.net:6200"
-}
-
 data "azurerm_key_vault" "sscs_key_vault" {
   name                = "${local.azureVaultName}"
   resource_group_name = "${local.azureVaultName}"
