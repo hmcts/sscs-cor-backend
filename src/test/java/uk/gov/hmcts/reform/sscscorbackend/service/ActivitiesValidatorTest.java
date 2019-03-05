83a22030795b99b0e7c33291ddfa76715e51ee0d
Chris Grimble <chris@cg-it-solutions.com>
2019-03-05 14:53:01 +0000

    Check activities and score are mapped
    We a tribunal view is issued we generate a PDF with the details in it.
    If we send an activity or score to the PDF service without a mapped
    value to display the PDF service fails with a very generic error. Added
    in checking that that we have mapped values in en.json for all
    activities and scores as we pass them to the PDF service.

---
+++ src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/ActivitiesValidatorTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.reform.sscscorbackend.service;
+
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+import org.junit.Test;
+import uk.gov.hmcts.reform.sscscorbackend.domain.*;
+import uk.gov.hmcts.reform.sscscorbackend.service.pdf.I18nBuilder;
+
+public class ActivitiesValidatorTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void failsIfDailyLivingActivityNotMapped() throws IOException {
+
+        I18nBuilder i18nBuilder = new I18nBuilder();
+
+        OnlineHearing onlineHearing = createOnlineHearing(new Activity("notMappedActivity", "1"), new Activity("movingAround", "0"));
+        new ActivitiesValidator(i18nBuilder).validateWeHaveMappingForActivities(onlineHearing);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsIfMobilityActivityNotMapped() throws IOException {
+
+        I18nBuilder i18nBuilder = new I18nBuilder();
+
+        OnlineHearing onlineHearing = createOnlineHearing(new Activity("makingBudgetingDecisions", "6"), new Activity("notMappedActivity", "0"));
+        new ActivitiesValidator(i18nBuilder).validateWeHaveMappingForActivities(onlineHearing);
+    }
+
+    @Test
+    public void passesIfAllActivitiesAndScoresMapped() throws IOException {
+        I18nBuilder i18nBuilder = new I18nBuilder();
+
+        OnlineHearing onlineHearing = createOnlineHearing(new Activity("makingBudgetingDecisions", "6"), new Activity("movingAround", "0"));
+        new ActivitiesValidator(i18nBuilder).validateWeHaveMappingForActivities(onlineHearing);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsIfDailyLivingActivityScoreNotMapped() throws IOException {
+
+        I18nBuilder i18nBuilder = new I18nBuilder();
+
+        OnlineHearing onlineHearing = createOnlineHearing(new Activity("makingBudgetingDecisions", "999"), new Activity("movingAround", "0"));
+        new ActivitiesValidator(i18nBuilder).validateWeHaveMappingForActivities(onlineHearing);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsIfMobilityActivityScoreNotMapped() throws IOException {
+
+        I18nBuilder i18nBuilder = new I18nBuilder();
+
+        OnlineHearing onlineHearing = createOnlineHearing(new Activity("makingBudgetingDecisions", "6"), new Activity("movingAround", "999"));
+        new ActivitiesValidator(i18nBuilder).validateWeHaveMappingForActivities(onlineHearing);
+    }
+
+    private OnlineHearing createOnlineHearing(Activity dailyLiving, Activity mobility) {
+        return new OnlineHearing(
+                "hearingId",
+                "appellantName",
+                "caseRef",
+                new Decision(
+                        "sate",
+                        "startTime",
+                        "appellantReply",
+                        "replyDate",
+                        "startDate",
+                        "endDate",
+                        mock(DecisionRates.class),
+                        "reason",
+                        new Activities(singletonList(dailyLiving), singletonList(mobility))
+                ),
+                new FinalDecision("reason"),
+                true
+        );
+    }
+
+}
