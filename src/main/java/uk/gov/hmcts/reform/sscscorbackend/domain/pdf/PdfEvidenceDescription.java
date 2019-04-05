cf65b99b6970e8002f4f855a105e2230a5e66d81
Chris Grimble <chris@cg-it-solutions.com>
2019-04-05 16:27:24 +0100

    SSCS-3568 Additional eveidence cover sheet
    Added tests for StoreEvidenceDescriptionService

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/domain/pdf/PdfEvidenceDescription.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/domain/pdf/PdfEvidenceDescription.java
@@ -24,4 +24,32 @@ public class PdfEvidenceDescription {
     public List<String> getFileNames() {
         return fileNames;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        PdfEvidenceDescription that = (PdfEvidenceDescription) o;
+
+        if (appealDetails != null ? !appealDetails.equals(that.appealDetails) : that.appealDetails != null) {
+            return false;
+        }
+        if (description != null ? !description.equals(that.description) : that.description != null) {
+            return false;
+        }
+        return fileNames != null ? fileNames.equals(that.fileNames) : that.fileNames == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = appealDetails != null ? appealDetails.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (fileNames != null ? fileNames.hashCode() : 0);
+        return result;
+    }
 }
---
+++ src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/StoreEvidenceDescriptionServiceTest.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.reform.sscscorbackend.service;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collections;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.service.CcdPdfService;
+import uk.gov.hmcts.reform.sscs.service.EvidenceManagementService;
+import uk.gov.hmcts.reform.sscscorbackend.domain.EvidenceDescription;
+import uk.gov.hmcts.reform.sscscorbackend.domain.pdf.PdfAppealDetails;
+import uk.gov.hmcts.reform.sscscorbackend.domain.pdf.PdfEvidenceDescription;
+import uk.gov.hmcts.reform.sscscorbackend.service.pdf.EvidenceDescriptionPdfData;
+import uk.gov.hmcts.reform.sscscorbackend.thirdparty.pdfservice.PdfService;
+
+public class StoreEvidenceDescriptionServiceTest {
+    private StoreEvidenceDescriptionService storeEvidenceDescriptionService;
+
+    @Before
+    public void setUp() throws Exception {
+        storeEvidenceDescriptionService = new StoreEvidenceDescriptionService(
+                mock(PdfService.class),
+                "template_path",
+                mock(CcdPdfService.class),
+                mock(IdamService.class),
+                mock(EvidenceManagementService.class)
+        );
+    }
+
+    @Test
+    public void pdfHasNotAlreadyBeenCreatedIsAlwaysTrue() {
+        assertTrue(storeEvidenceDescriptionService.pdfHasNotAlreadyBeenCreated(mock(SscsCaseDetails.class), "docPrefix"));
+    }
+
+    @Test
+    public void documentNamePrefixIsEvidenceDescription() {
+        assertThat(storeEvidenceDescriptionService.documentNamePrefix(mock(SscsCaseDetails.class), "hearingId"), is("Evidence Description - "));
+    }
+
+    @Test
+    public void canGetPdfContent() {
+        PdfAppealDetails appealDetails = mock(PdfAppealDetails.class);
+        List<String> fileName = Collections.singletonList("fileName");
+        String description = "description";
+        EvidenceDescriptionPdfData evidenceDescriptionPdfData = new EvidenceDescriptionPdfData(mock(SscsCaseDetails.class), new EvidenceDescription(description), fileName);
+        PdfEvidenceDescription pdfContent = storeEvidenceDescriptionService.getPdfContent(evidenceDescriptionPdfData, "hearingId", appealDetails);
+
+        assertThat(pdfContent, is(new PdfEvidenceDescription(appealDetails, description, fileName)));
+    }
+}
