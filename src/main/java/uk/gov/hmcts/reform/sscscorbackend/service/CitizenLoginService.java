cf83a3c59a33a5c97721a7aacb00033dd2a4206d
chrisgrimble <emailme@chrisgrimble.com>
2019-07-12 10:06:05 +0100

    Add extra logging to the citizen login.

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/CitizenLoginService.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/CitizenLoginService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscscorbackend.service;
 
+import static java.lang.String.format;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Stream.of;
 import static org.apache.commons.lang3.StringUtils.isBlank;
@@ -7,6 +8,7 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
@@ -18,6 +20,7 @@ import uk.gov.hmcts.reform.sscscorbackend.domain.OnlineHearing;
 import uk.gov.hmcts.reform.sscscorbackend.thirdparty.ccd.CorCcdService;
 import uk.gov.hmcts.reform.sscscorbackend.util.PostcodeUtil;
 
+@Slf4j
 @Service
 public class CitizenLoginService {
     private final CorCcdService corCcdService;
@@ -35,19 +38,27 @@ public class CitizenLoginService {
     }
 
     public List<OnlineHearing> findCasesForCitizen(IdamTokens idamTokens, String tya) {
+        log.info(format("Find case: Searching for case with tya [%s] for user [%s]", tya, idamTokens.getUserId()));
         List<CaseDetails> caseDetails = corCcdService.searchForCitizen(idamTokens);
         List<SscsCaseDetails> sscsCaseDetails = caseDetails.stream()
                 .map(sscsCcdConvertService::getCaseDetails)
                 .collect(toList());
         if (!isBlank(tya)) {
-            return convert(
+            log.info(format("Find case: Filtering for case with tya [%s] for user [%s]", tya, idamTokens.getUserId()));
+            List<OnlineHearing> convert = convert(
                     sscsCaseDetails.stream()
                             .filter(casesWithSubscriptionMatchingTya(tya))
                             .collect(toList())
             );
+            log.info(format("Find case: Found [%s] cases for tya [%s] for user [%s]", convert.size(), tya, idamTokens.getUserId()));
+
+            return convert;
         }
 
-        return convert(sscsCaseDetails);
+        log.info(format("Searching for case without for user [%s]", idamTokens.getUserId()));
+        List<OnlineHearing> convert = convert(sscsCaseDetails);
+        log.info(format("Found [%s] cases without tya for user [%s]", convert.size(), idamTokens.getUserId()));
+        return convert;
     }
 
     private List<OnlineHearing> convert(List<SscsCaseDetails> sscsCaseDetails) {
@@ -61,13 +72,25 @@ public class CitizenLoginService {
     public Optional<OnlineHearing> associateCaseToCitizen(IdamTokens citizenIdamTokens, String tya, String email, String postcode) {
         SscsCaseDetails caseByAppealNumber = corCcdService.findCaseByAppealNumber(tya, idamService.getIdamTokens());
 
-        if (caseByAppealNumber != null && postcodeUtil.hasAppellantPostcode(caseByAppealNumber, postcode) && caseHasSubscriptionWithTyaAndEmail(caseByAppealNumber, tya, email)) {
-            corCcdService.addUserToCase(citizenIdamTokens.getUserId(), caseByAppealNumber.getId());
+        if (caseByAppealNumber != null) {
+            log.info(format("Associate case: Found case to assign id [%s] for tya [%s] email [%s] postcode [%s]", caseByAppealNumber.getId(), tya, email, postcode));
+            if (postcodeUtil.hasAppellantPostcode(caseByAppealNumber, postcode)) {
+                log.info(format("Associate case: Found case to assign id [%s] for tya [%s] email [%s] postcode [%s] matches postcode", caseByAppealNumber.getId(), tya, email, postcode));
+                if (caseHasSubscriptionWithTyaAndEmail(caseByAppealNumber, tya, email)) {
+                    log.info(format("Found case to assign id [%s] for tya [%s] email [%s] postcode [%s] has subscription", caseByAppealNumber.getId(), tya, email, postcode));
+                    corCcdService.addUserToCase(citizenIdamTokens.getUserId(), caseByAppealNumber.getId());
 
-            return onlineHearingService.loadHearing(caseByAppealNumber);
+                    return onlineHearingService.loadHearing(caseByAppealNumber);
+                } else {
+                    log.info(format("Associate case: Subscription does not match id [%s] for tya [%s] email [%s] postcode [%s]", caseByAppealNumber.getId(), tya, email, postcode));
+                }
+            } else {
+                log.info(format("Associate case: Postcode does not match id [%s] for tya [%s] email [%s] postcode [%s]", caseByAppealNumber.getId(), tya, email, postcode));
+            }
         } else {
-            return Optional.empty();
+            log.info(format("Associate case: No case found for tya [%s] email [%s] postcode [%s]", tya, email, postcode));
         }
+        return Optional.empty();
     }
 
     private Predicate<SscsCaseDetails> casesWithSubscriptionMatchingTya(String tya) {
