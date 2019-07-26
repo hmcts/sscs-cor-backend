32adeec9215b3aa0956e83c02a1a5ba1910258ec
chrisgrimble <emailme@chrisgrimble.com>
2019-07-26 11:30:07 +0100

    SSCS-6098 Update get coversheet endpoint
    Previously you had to have a hearing id to get a cover sheet. Now with
    MYA you might not have a hearing ID. so need to create the coversheet
    with the case id. Update the coversheet endpoint to take either a case
    id or hearing id. As the case id is a long and the hearing id a GUUID
    we can tell which we have been given.

--- src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/coversheet/CoversheetServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscscorbackend/service/coversheet/CoversheetServiceTest.java
@@ -9,15 +9,22 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 import uk.gov.hmcts.reform.sscscorbackend.service.OnlineHearingService;
 import uk.gov.hmcts.reform.sscscorbackend.thirdparty.pdfservice.PdfService;
 
 public class CoversheetServiceTest {
 
     private String onlineHearingId;
+    private long caseId = 1234567890L;
     private OnlineHearingService onlineHearingService;
     private PdfService pdfService;
     private String template;
+    private CcdService ccdService;
+    private IdamService idamService;
+    private IdamTokens idamTokens;
 
     @Before
     public void setUp() {
@@ -25,6 +32,10 @@ public class CoversheetServiceTest {
         onlineHearingService = mock(OnlineHearingService.class);
         pdfService = mock(PdfService.class);
         template = "template";
+        ccdService = mock(CcdService.class);
+        idamService = mock(IdamService.class);
+        idamTokens = IdamTokens.builder().build();
+        when(idamService.getIdamTokens()).thenReturn(idamTokens);
     }
 
     @Test
@@ -37,7 +48,7 @@ public class CoversheetServiceTest {
         when(pdfService.createPdf(pdfSummary, template)).thenReturn(pdf);
 
         Optional<byte[]> pdfOptional =
-                new CoversheetService(onlineHearingService, pdfService, template).createCoverSheet(onlineHearingId);
+                new CoversheetService(onlineHearingService, pdfService, template, ccdService, idamService).createCoverSheet(onlineHearingId);
 
         assertThat(pdfOptional.isPresent(), is(true));
         assertThat(pdfOptional.get(), is(pdf));
@@ -48,7 +59,32 @@ public class CoversheetServiceTest {
         when(onlineHearingService.getCcdCase(onlineHearingId)).thenReturn(Optional.empty());
 
         Optional<byte[]> pdfOptional =
-                new CoversheetService(onlineHearingService, pdfService, template).createCoverSheet(onlineHearingId);
+                new CoversheetService(onlineHearingService, pdfService, template, ccdService, idamService).createCoverSheet(onlineHearingId);
+
+        assertThat(pdfOptional.isPresent(), is(false));
+    }
+
+    @Test
+    public void canLoadCcdCaseByCaseIdAndProducePdf() {
+        when(ccdService.getByCaseId(caseId, idamTokens)).thenReturn(createSscsCaseDetails());
+
+        byte[] pdf = {2, 4, 6, 0, 1};
+        PdfCoverSheet pdfSummary = new PdfCoverSheet("12345", "firstname lastname", "line1", "line2", "town", "county", "postcode");
+        when(pdfService.createPdf(pdfSummary, template)).thenReturn(pdf);
+
+        Optional<byte[]> pdfOptional =
+                new CoversheetService(onlineHearingService, pdfService, template, ccdService, idamService).createCoverSheet(caseId + "");
+
+        assertThat(pdfOptional.isPresent(), is(true));
+        assertThat(pdfOptional.get(), is(pdf));
+    }
+
+    @Test
+    public void cannotLoadCcdCaseByCaseId() {
+        when(ccdService.getByCaseId(caseId, idamTokens)).thenReturn(null);
+
+        Optional<byte[]> pdfOptional =
+                new CoversheetService(onlineHearingService, pdfService, template, ccdService, idamService).createCoverSheet(onlineHearingId);
 
         assertThat(pdfOptional.isPresent(), is(false));
     }
