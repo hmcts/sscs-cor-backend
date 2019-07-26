e3ccb5a540d05adae9a96547d695e70cd092c9b2
chrisgrimble <emailme@chrisgrimble.com>
2019-07-26 11:30:07 +0100

    SSCS-6098 Update get coversheet endpoint
    Previously you had to have a hearing id to get a cover sheet. Now with
    MYA you might not have a hearing ID. so need to create the coversheet
    with the case id. Update the coversheet endpoint to take either a case
    id or hearing id. As the case id is a long and the hearing id a GUUID
    we can tell which we have been given.

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/coversheet/CoversheetService.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/service/coversheet/CoversheetService.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.sscscorbackend.service.coversheet;
 
 import java.util.Optional;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Address;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscscorbackend.service.OnlineHearingService;
 import uk.gov.hmcts.reform.sscscorbackend.thirdparty.pdfservice.PdfService;
 
@@ -14,18 +18,26 @@ public class CoversheetService {
     private final OnlineHearingService onlineHearingService;
     private final PdfService pdfService;
     private final String template;
+    private final CcdService ccdService;
+    private final IdamService idamService;
 
     public CoversheetService(
             OnlineHearingService onlineHearingService,
             @Qualifier("docmosisPdfService") PdfService pdfService,
-            @Value("${evidenceCoverSheet.docmosis.template}") String template) {
+            @Value("${evidenceCoverSheet.docmosis.template}") String template, CcdService ccdService, IdamService idamService) {
         this.onlineHearingService = onlineHearingService;
         this.pdfService = pdfService;
         this.template = template;
+        this.ccdService = ccdService;
+        this.idamService = idamService;
     }
 
-    public Optional<byte[]> createCoverSheet(String onlineHearingId) {
-        return onlineHearingService.getCcdCase(onlineHearingId)
+    public Optional<byte[]> createCoverSheet(String identifier) {
+        Optional<SscsCaseDetails> sscsCaseDetails = !NumberUtils.isDigits(identifier)
+                ? onlineHearingService.getCcdCase(identifier) :
+                Optional.ofNullable(ccdService.getByCaseId(Long.parseLong(identifier), idamService.getIdamTokens()));
+
+        return sscsCaseDetails
                 .map(sscsCase -> {
                     SscsCaseData sscsCaseData = sscsCase.getData();
                     Address address = sscsCaseData.getAppeal().getAppellant().getAddress();
