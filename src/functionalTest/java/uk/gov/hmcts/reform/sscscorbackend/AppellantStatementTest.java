a0e8ecc0230be7c23c529ad9c54bbf672f2ed83c
Chris <christopher.davidson@btinternet.com>
2020-01-14 18:29:43 +0000

    added functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/AppellantStatementTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscscorbackend/AppellantStatementTest.java
@@ -8,6 +8,13 @@ public class AppellantStatementTest extends BaseFunctionTest {
     public void canUploadAnAppellantStatement() throws IOException {
         OnlineHearing hearingWithQuestion = createHearing(true);
 
-        sscsCorBackendRequests.uploadAppellantStatement(hearingWithQuestion.getCaseId(), "statement");
+        sscsCorBackendRequests.uploadAppellantStatement(hearingWithQuestion.getHearingId(), "statement");
+    }
+
+    @Test
+    public void canUploadAnAppellantStatementForDigital() throws IOException {
+        CreatedCcdCase createdCase = createCase();
+
+        sscsCorBackendRequests.uploadAppellantStatement(createdCase.getCaseId(), "statement");
     }
 }
