51a47e1d493011eaae4fce2fbe6ed8fc93ea6ffd
srochani <sunil.rochani@hotmail.com>
2020-04-23 21:52:03 +0100

    Fixed unsuccessfull authentication

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/config/SecurityConfiguration.java
@@ -6,22 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final ServiceAuthFilter serviceAuthFilter;
-
-    public SecurityConfiguration(ServiceAuthFilter serviceAuthFilter) {
-        this.serviceAuthFilter = serviceAuthFilter;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.addFilter(serviceAuthFilter)
+        http
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
                 .csrf().disable()
