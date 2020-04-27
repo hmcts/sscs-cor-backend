3c239632fb82fdbd08860454bf0b301b01e248a4
srochani <sunil.rochani@hotmail.com>
2020-04-27 22:59:35 +0100

    Fixed unsuccessfull authentication

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/config/SecurityConfiguration.java
@@ -2,19 +2,36 @@ package uk.gov.hmcts.reform.sscscorbackend.config;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.web.filter.OncePerRequestFilter;
+import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final OncePerRequestFilter oncePerRequestFilter;
+
+    @Autowired
+    public SecurityConfiguration(ServiceAuthFilter oncePerRequestFilter) {
+        this.oncePerRequestFilter = oncePerRequestFilter;
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().anyRequest();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
+        http.addFilterBefore(oncePerRequestFilter, FilterSecurityInterceptor.class)
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
                 .csrf().disable()
